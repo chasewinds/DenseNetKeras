0fcca57843805829c90b5fc12d045da661d94549
Developer <rubans@Rubans.local>
2018-02-27 00:04:35 +0800

    2.26 commit

--- data/get_datasets.sh
+++ data/get_datasets.sh
@@ -1,14 +1,14 @@
-mkdir -p cifar10
-#CIFAR-10 dataset
-wget -O cifar10/cifar.tar.gz http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xzvf cifar10/cifar.tar.gz -C cifar10 --strip-components=1
-rm cifar10/cifar.tar.gz
+# mkdir -p cifar10
+# #CIFAR-10 dataset
+# wget -O cifar10/cifar.tar.gz http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+# tar -xzvf cifar10/cifar.tar.gz -C cifar10 --strip-components=1
+# rm cifar10/cifar.tar.gz
 
-#CIFAR-100 dataset
-mkdir -p cifar100
-wget -O cifar100/cifar100.tar.gz https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz
-tar -xzvf cifar100/cifar100.tar.gz -C cifar100 --strip-components=1
-rm cifar100/cifar100.tar.gz
+# #CIFAR-100 dataset
+# mkdir -p cifar100
+# wget -O cifar100/cifar100.tar.gz https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz
+# tar -xzvf cifar100/cifar100.tar.gz -C cifar100 --strip-components=1
+# rm cifar100/cifar100.tar.gz
 
 mkdir -p mnist
 #MNIST dataset
@@ -17,8 +17,8 @@ wget -O mnist/train-labels-idx1-ubyte.gz http://yann.lecun.com/exdb/mnist/train-
 wget -O mnist/t10k-images-idx3-ubyte.gz http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
 wget -O mnist/t10k-labels-idx1-ubyte.gz http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
 
-mkdir -p svhn
-#SVHN dataset
-wget -O svhn/train_32x32.mat http://ufldl.stanford.edu/housenumbers/train_32x32.mat
-wget -O svhn/test_32x32.mat http://ufldl.stanford.edu/housenumbers/test_32x32.mat
-wget -O svhn/extra_32x32.mat http://ufldl.stanford.edu/housenumbers/extra_32x32.mat
+# mkdir -p svhn
+# #SVHN dataset
+# wget -O svhn/train_32x32.mat http://ufldl.stanford.edu/housenumbers/train_32x32.mat
+# wget -O svhn/test_32x32.mat http://ufldl.stanford.edu/housenumbers/test_32x32.mat
+# wget -O svhn/extra_32x32.mat http://ufldl.stanford.edu/housenumbers/extra_32x32.mat
